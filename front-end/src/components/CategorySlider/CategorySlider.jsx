9f8b3b6e3faa0c59c76adcf8abc52bac058738ca
CyprianRachel <cyprianrachel@gmail.com>
2024-06-18 20:45:56 +0200

    CategorySlider component

--- front-end/src/components/CategorySlider/CategorySlider.jsx
+++ front-end/src/components/CategorySlider/CategorySlider.jsx
@@ -9,22 +9,42 @@ export function CategorySlider() {
       <CenteredContent>
         <div className={styles.mainCategories}>
           {HEROCATEGORIES.map((category) => {
+            const words = category.categoryTitle.split(" ");
             return (
               <div
                 key={category.categoryTitle}
                 className={styles.singleCategoryWrapper}
               >
                 <div className={styles.leftDiv}>
-                  <h2>{category.categoryTitle}</h2>
+                  <h2>
+                    {words.map((word, index) => (
+                      <span
+                        key={index}
+                        className={index === 0 ? styles.black : styles.orange}
+                      >
+                        {word}{" "}
+                      </span>
+                    ))}
+                  </h2>
                   <p>{category.description}</p>
                 </div>
                 <div className={styles.rightDiv}>
                   <img
-                    style={{ marginLeft: "auto", width: "3rem" }}
+                    style={{ marginLeft: "auto", width: "2.5rem" }}
                     src={UPRIGHTICON}
                   ></img>
                   <div className={styles.imageWrapper}>
-                    <img src={category.image}></img>
+                    <div className={styles.whiteCircle}></div>
+                    <div className={styles.orangeCircle}></div>
+                    <div
+                      style={{
+                        backgroundImage: `url(${category.image})`,
+                        width: "100%",
+                        height: "12rem",
+                        position: "relative",
+                        backgroundRepeat: "no-repeat",
+                      }}
+                    ></div>
                   </div>
                 </div>
               </div>
