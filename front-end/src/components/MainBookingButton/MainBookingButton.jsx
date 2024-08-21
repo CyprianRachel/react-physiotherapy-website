991c6f64ca98dc1b3e041fc6a76424a3f2a83886
CyprianRachel <cyprianrachel@gmail.com>
2024-08-21 22:13:38 +0200

    Button Booksy

--- front-end/src/components/MainBookingButton/MainBookingButton.jsx
+++ front-end/src/components/MainBookingButton/MainBookingButton.jsx
@@ -1,9 +1,27 @@
+import { useEffect } from "react";
 import styles from "./MainBookingButton.module.css";
 
 export function MainBookingButton() {
+  useEffect(() => {
+    // Tworzymy element <script>
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src =
+      "https://booksy.com/widget/code.js?id=119503&country=pl&lang=pl";
+    script.async = true;
+
+    // Dodajemy skrypt do kontenera w dokumencie
+    document.getElementById("booking-container").appendChild(script);
+
+    // Opcjonalnie, usuwamy skrypt po demontażu komponentu
+    return () => {
+      document.getElementById("booking-container").removeChild(script);
+    };
+  }, []);
+
   return (
-    <a href="tel:+48660143444">
-      <button className={styles.MainBookingButton}>Zarezerwuj</button>
-    </a>
+    <div>
+      <div id="booking-container"></div>
+    </div>
   );
 }
