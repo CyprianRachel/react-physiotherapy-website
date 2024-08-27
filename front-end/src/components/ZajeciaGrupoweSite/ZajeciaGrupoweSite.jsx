8989222665544c20cd444fcd876e1449a107f9d7
CyprianRachel <cyprianrachel@gmail.com>
2024-08-27 20:02:45 +0200

    bestter

--- front-end/src/components/ZajeciaGrupoweSite/ZajeciaGrupoweSite.jsx
+++ front-end/src/components/ZajeciaGrupoweSite/ZajeciaGrupoweSite.jsx
@@ -1,23 +1,19 @@
 import { Link } from "react-router-dom";
 import { ZAJECIA_GRUPOWE } from "../../constants/zajeciaGrupowe";
 import styles from "./ZajeciaGrupoweSite.module.css";
+import { H1 } from "../H1/H1";
 
 export function ZajeciaGrupoweSite() {
   return (
-    <div className={styles.wrapper}>
-      <h1>
-        Zajęcia <span className={styles.orange}>grupowe</span>
-      </h1>
+    <>
+      <H1 text={"Zajęcia"} highlight={"grupowe"}></H1>
       <div className={styles.membersWrapper}>
         {ZAJECIA_GRUPOWE.map((employee) => {
           return (
             <Link to={employee.path} key={employee.name}>
               <div className={styles.singleMemberWrapper}>
-                <div className={styles.employeeImageWrapper}>
-                  <img className={styles.employeeImage} src={employee.image} />
-                </div>
+                <img className={styles.employeeImage} src={employee.image} />
                 <div className={styles.description}>
-                  <p>{employee.profession}</p>
                   <h2>{employee.name}</h2>
                   <button>Zobacz zajęcia</button>
                 </div>
@@ -26,6 +22,6 @@ export function ZajeciaGrupoweSite() {
           );
         })}
       </div>
-    </div>
+    </>
   );
 }
