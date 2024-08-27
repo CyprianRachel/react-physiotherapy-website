b3ca7a20fe926efbc87a45a6ff0d1ccf386d8c6b
CyprianRachel <cyprianrachel@gmail.com>
2024-08-27 17:21:41 +0200

    Our Team

--- front-end/src/components/NaszZespol/NaszZespol.jsx
+++ front-end/src/components/NaszZespol/NaszZespol.jsx
@@ -2,31 +2,33 @@ import { Link } from "react-router-dom";
 import { TEAMMEMBERS } from "../../constants/teamMembers";
 import styles from "./NaszZespol.module.css";
 import { scrollToTop } from "../../hooks/scrollToTop";
+import UP_RIGHT_ICON from "../../assets/up-right-icon.svg";
 
 export function NaszZespol() {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.membersWrapper}>
-        {TEAMMEMBERS.map((employee) => {
-          return (
-            <Link to={employee.path} key={employee.name} onClick={scrollToTop}>
-              <div className={styles.singleMemberWrapper}>
-                <div
-                  className={styles.employeeImage}
-                  style={{
-                    backgroundImage: `url(${employee.image})`,
-                  }}
-                ></div>
-                <div className={styles.description}>
-                  <p>{employee.profession}</p>
-                  <h2>{employee.name}</h2>
-                  <button>Zobacz usługi</button>
-                </div>
-              </div>
-            </Link>
-          );
-        })}
-      </div>
+    <div className={styles.membersWrapper}>
+      {TEAMMEMBERS.map((employee) => {
+        return (
+          <Link
+            className={styles.singleMemberWrapper}
+            to={employee.path}
+            key={employee.name}
+            onClick={scrollToTop}
+          >
+            <div
+              className={styles.employeeImage}
+              style={{
+                backgroundImage: `url(${employee.image})`,
+              }}
+            ></div>
+            <div className={styles.description}>
+              <h2>{employee.name}</h2>
+              <h3>{employee.profession}</h3>
+            </div>
+            <img src={UP_RIGHT_ICON} className={styles.img}></img>
+          </Link>
+        );
+      })}
     </div>
   );
 }
