b224f7d4d7c0c3fe51ec82c4fa48ec0c8ebd27b9
CyprianRachel <cyprianrachel@gmail.com>
2024-08-30 02:20:19 +0200

    LAst

--- front-end/src/components/NothingFound/NothingFound.jsx
+++ front-end/src/components/NothingFound/NothingFound.jsx
@@ -5,11 +5,13 @@ import { scrollToTop } from "../../hooks/scrollToTop";
 
 export function NothingFound() {
   return (
-    <div className={styles.wrapper}>
-      <img className={styles.img} src={NO_RESULTS} />
-      <div className={styles.containerH3}>
-        <h3>Brak wyników</h3>
-        <p>Żadna z usług nie spełnia twoich kryteriów wyszukiwania</p>
+    <div className={styles.wrapperAll}>
+      <div className={styles.wrapper}>
+        <img className={styles.img} src={NO_RESULTS} />
+        <div className={styles.containerH3}>
+          <h3>Brak wyników</h3>
+          <p>Żadna z usług nie spełnia twoich kryteriów wyszukiwania</p>
+        </div>
       </div>
     </div>
   );
