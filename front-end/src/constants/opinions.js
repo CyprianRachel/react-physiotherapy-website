a5c9d37b70ade6bd3f8aef9e94b7ebedaa6a92f9
CyprianRachel <cyprianrachel@gmail.com>
2024-08-30 02:28:53 +0200

    Best

--- front-end/src/constants/opinions.js
+++ front-end/src/constants/opinions.js
@@ -5,14 +5,14 @@ export const OPINIONS = [
   {
     id: 1,
     average: "5.0",
-    counter: 15,
+    counter: 17,
     image: GOOGLE_LOGO,
     path: "https://www.google.com/search?sca_esv=4bb6b1564136f20c&sca_upv=1&rlz=1C1CHBD_plPL1096PL1097&tbm=lcl&sxsrf=ADLYWIJY-PuPN762G0OGE-6MzQrChoUjEw:1719960758428&q=in.motion+Opinie&rflfq=1&num=20&stick=H4sIAAAAAAAAAONgkxIxNDQ2sDAyNDa3MDAGAgtTY1PTDYyMrxgFMvP0cvNLMvPzFPwLMvMyUxexYggBAJsEKb5BAAAA&rldimm=11308213780333385355&hl=pl-PL&sa=X&ved=2ahUKEwjjo6KQuYmHAxW0SfEDHY7UD-oQ9fQKegQIHRAF&biw=1920&bih=991&dpr=1#lkt=LocalPoiReviews",
   },
   {
     id: 2,
     average: "5.0",
-    counter: 53,
+    counter: 72,
     image: BOOKSY_LOGO,
     path: "https://booksy.com/pl-pl/119503_in-motion-fizjoterapia-i-terapia-si_fizjoterapia_12321_swietochlowice#ba_s=seo",
   },
